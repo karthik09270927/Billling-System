ff1dc2bc6a4c55d6d1e0f912524f05c365d3c5b7
Manikandaprabu Subramanian <manikandan.subramanian@coherent.in>
2025-01-25 10:28:27 +0530

    admin Dashboard add Category

--- src/components/AdminHeader.tsx
+++ src/components/AdminHeader.tsx
@@ -9,6 +9,13 @@ import {
     Card,
     CardContent,
     CardMedia,
+    Button,
+    CircularProgress,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogTitle,
+    Modal,
 } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
@@ -18,18 +25,30 @@ import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import ExpandLessIcon from '@mui/icons-material/ExpandLess';
 import { useNavigate } from "react-router-dom";
 import { fetchCategories, fetchSubCategories } from "../utils/api-collection";
-import LogoutIcon from "@mui/icons-material/Logout";
-import QrCodeScannerIcon from "@mui/icons-material/QrCodeScanner";
+import EditIcon from '@mui/icons-material/Edit'
+import {
 
+    List,
+    ListItem,
+    ListItemText,
+    ListItemSecondaryAction,
+} from "@mui/material";
+import { Add as AddIcon, Delete as DeleteIcon } from "@mui/icons-material";
 
 
 const AdminHeader = () => {
     const [isHeaderOpen, setIsHeaderOpen] = useState(true);
+    const [editProduct, setEditProduct] = useState(false);
     const [categories, setCategories] = useState<any[]>([]);
     const scrollContainerRef = useRef<HTMLDivElement>(null);
+    const [isModalOpen, setIsModalOpen] = useState(false);
     const { selectedCategory, setSelectedCategory, setSubCategories } = useCategory();
     const navigate = useNavigate();
-
+    const [uploadedImage, setUploadedImage] = useState<string | null>(null);
+    const [itemName, setItemName] = useState("");
+    const [subCategoryName, setSubCategoryName] = useState("");
+    const [subCategory, setSubCategory] = useState<string[]>([]);
+    const [step, setStep] = useState(1); // Step tracker
 
     useEffect(() => {
         const getCategories = async () => {
@@ -58,11 +77,37 @@ const AdminHeader = () => {
         }
     };
 
-    const handleLogout = () => {
-        localStorage.removeItem('userRole');
-        localStorage.removeItem('accessToken');
-        localStorage.removeItem('refreshToken');
-        navigate('/');
+    const handleShowEdit = () => {
+        setEditProduct((prev) => !prev);
+        console.log(editProduct);
+
+    };
+
+    const handleAddItem = () => {
+
+
+    };
+
+    const handleCloseModal = () => {
+        setIsModalOpen(false);
+        setUploadedImage(null);
+        setItemName("");
+        setSubCategoryName("");
+        setSubCategory([]);
+        setStep(1);
+
+    };
+
+    const handleEdit = () => {
+        setIsModalOpen(true);
+        console.log("edit");
+
+    };
+
+
+    const handleDelete = () => {
+        console.log("delete");
+
     };
 
     const handleCategoryClick = async (categoryId: number, categoryName: string) => {
@@ -74,6 +119,68 @@ const AdminHeader = () => {
             console.error("Error fetching subcategories:", error);
         }
     };
+
+
+
+    const handleSave = () => {
+        console.log("Item Name:", itemName);
+        console.log("Uploaded Image:", uploadedImage);
+        console.log("Category:", subCategoryName);
+        setIsModalOpen(false);
+        setUploadedImage(null);
+        setItemName("");
+        setSubCategoryName("");
+
+    };
+
+
+    const handleImageUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (e.target.files && e.target.files[0]) {
+            const reader = new FileReader();
+            reader.onload = (event) => {
+                if (event.target) {
+                    setUploadedImage(event.target.result as string);
+                }
+            };
+            reader.readAsDataURL(e.target.files[0]);
+        }
+    };
+
+    const handleAddSubCategory = () => {
+        if (subCategoryName) {
+            setSubCategory([...subCategory, subCategoryName]);
+            setSubCategoryName("");
+        }
+    };
+
+
+    const handleRemoveSubCategory = (index: number) => {
+        setSubCategory(subCategory.filter((_, i) => i !== index));
+    };
+
+    const handleSubmit = () => {
+        console.log("Category Name:", itemName);
+        console.log("Uploaded Image:", uploadedImage);
+        console.log("Sub Categories:", subCategory);
+
+        // Reset all values
+        setIsModalOpen(false);
+        setUploadedImage(null);
+        setItemName("");
+        setSubCategoryName("");
+        setSubCategory([]);
+        setStep(1);
+    };
+
+    const handleNext = () => {
+        if (step === 1) {
+            setStep(2);
+        }
+    };
+
+
+
+
 
 
     return (
@@ -139,6 +246,19 @@ const AdminHeader = () => {
                         {/* Search Bar, QR Code, and Logout Icons */}
                         <Grid item xs>
                             <Box sx={{ display: "flex", justifyContent: "flex-end", gap: 1 }}>
+                                <IconButton
+                                    onClick={handleShowEdit}
+                                    sx={{
+                                        backgroundColor: "rgb(238, 255, 226)",
+                                        borderRadius: "20px",
+                                        "& .MuiOutlinedInput-root": {
+                                            "& fieldset": {
+                                                border: "none",
+                                            },
+                                        },
+                                    }}>
+                                    <EditIcon />
+                                </IconButton>
                                 <TextField
                                     placeholder="Search something sweet on your mind..."
                                     variant="outlined"
@@ -150,7 +270,7 @@ const AdminHeader = () => {
                                         ),
                                     }}
                                     sx={{
-                                            backgroundColor: "rgb(238, 255, 226)",
+                                        backgroundColor: "rgb(238, 255, 226)",
                                         borderRadius: "20px",
                                         "& .MuiOutlinedInput-root": {
                                             "& fieldset": {
@@ -159,12 +279,7 @@ const AdminHeader = () => {
                                         },
                                     }}
                                 />
-                                <IconButton>
-                                    <QrCodeScannerIcon sx={{ fontSize: 28, color: "#333" }} />
-                                </IconButton>
-                                <IconButton>
-                                    <LogoutIcon sx={{ fontSize: 28, color: "red" }} onClick={handleLogout} />
-                                </IconButton>
+
                             </Box>
                         </Grid>
                     </Grid>
@@ -210,8 +325,30 @@ const AdminHeader = () => {
                                         backgroundColor: selectedCategory === category.categoryName ? "rgb(238, 255, 226)" : "#F9F9F9",
                                         cursor: "pointer",
                                         transition: "box-shadow 0.3s",
+                                        position: "relative",
                                     }}
                                 >
+
+
+                                    {editProduct && (
+                                        <IconButton
+                                            onClick={handleEdit}
+                                            sx={{
+                                                position: "absolute",
+                                                borderRadius: "0% 30% 0% 70%",
+                                                right: "0px",
+                                                backgroundColor: "#74d52b",
+                                                zIndex: 1,
+                                                '&:hover': {
+                                                    backgroundColor: "rgb(169, 252, 91)",
+                                                },
+                                            }}
+                                        >
+                                            <EditIcon sx={{ fontSize: "18px", color: "#fff" }} />
+                                        </IconButton>
+                                    )
+                                    }
+
                                     <CardMedia
                                         component="img"
                                         height="90"
@@ -235,6 +372,42 @@ const AdminHeader = () => {
                                     </CardContent>
                                 </Card>
                             ))}
+
+                            <Card
+                                onClick={() => setIsModalOpen(true)}
+                                sx={{
+                                    minWidth: "120px",
+                                    maxWidth: "140px",
+                                    height: "150px",
+                                    borderRadius: "12px",
+                                    boxShadow: "0 2px 5px rgba(0, 0, 0, 0.1)",
+                                    backgroundColor: "#F9F9F9",
+                                    cursor: "pointer",
+                                    transition: "box-shadow 0.3s",
+                                }}
+                            >
+                                <Box
+                                    sx={{
+                                        height: "90px",
+                                        display: "flex",
+                                        alignItems: "center",
+                                        justifyContent: "center",
+                                        backgroundColor: "#F9F9F9",
+                                        borderRadius: "12px 12px 0 0",
+                                    }}
+                                >
+                                    <AddIcon sx={{ fontSize: "60px", color: "#74d52b" }} />
+                                </Box>
+
+                                <CardContent sx={{ textAlign: "center", padding: "8px 0" }}>
+                                    <Typography variant="body2" sx={{ fontWeight: "bold", color: "#333" }}>
+                                        Add Item
+                                    </Typography>
+
+                                </CardContent>
+                            </Card>
+
+
                         </Box>
                         {/* Right Arrow Icon */}
                         <IconButton onClick={() => scrollHorizontally("right")} sx={{ color: "#333" }}>
@@ -243,6 +416,161 @@ const AdminHeader = () => {
                     </Box>
                 )}
             </Box>
+            <Modal
+                open={isModalOpen}
+                onClose={handleCloseModal}
+                sx={{ display: "flex", alignItems: "center", justifyContent: "center" }}
+            >
+                <Box
+                    sx={{
+                        backgroundColor: "#FBFBE5",
+                        borderRadius: "8px",
+                        boxShadow: 24,
+                        p: 4,
+                        width: "400px",
+                        textAlign: "center",
+                    }}
+                >
+                    {step === 1 && (
+                        <>
+                            <Typography
+                                variant="h6"
+                                sx={{ textAlign: "center", mb: 3, fontWeight: "bold", color: "#333" }}
+                            >
+                                Add Category
+                            </Typography>
+
+                            {/* Image Upload */}
+                            <Box
+                                sx={{
+                                    display: "flex",
+                                    alignItems: "center",
+                                    justifyContent: "center",
+                                    flexDirection: "column",
+                                    mb: 3,
+                                }}
+                            >
+                                <input
+                                    accept="image/*"
+                                    type="file"
+                                    id="upload-image"
+                                    style={{ display: "none" }}
+                                    onChange={handleImageUpload}
+                                />
+                                <label htmlFor="upload-image">
+                                    <Box
+                                        sx={{
+                                            width: "150px",
+                                            height: "150px",
+                                            backgroundColor: "#F0F0F0",
+                                            borderRadius: "8px",
+                                            display: "flex",
+                                            alignItems: "center",
+                                            justifyContent: "center",
+                                            cursor: "pointer",
+                                            border: "1px dashed #74d52b",
+                                            overflow: "hidden",
+                                        }}
+                                    >
+                                        {uploadedImage ? (
+                                            <img
+                                                src={uploadedImage}
+                                                alt="Uploaded"
+                                                style={{ width: "100%", height: "100%", objectFit: "cover" }}
+                                            />
+                                        ) : (
+                                            <AddIcon sx={{ fontSize: "50px", color: "#74d52b" }} />
+                                        )}
+                                    </Box>
+                                </label>
+                                {!uploadedImage && (
+                                    <Typography variant="body2" sx={{ mt: 1, color: "#555" }}>
+                                        Upload Category Image
+                                    </Typography>
+                                )}
+                            </Box>
+
+                            {/* Item Name Input */}
+                            <TextField
+                                fullWidth
+                                label="Category Name"
+                                variant="outlined"
+                                value={itemName}
+                                onChange={(e) => setItemName(e.target.value)}
+                                sx={{ mb: 3 }}
+                            />
+
+                            {/* Next Button */}
+                            <Button
+                                variant="contained"
+                                color="success"
+                                fullWidth
+                                onClick={handleNext}
+                                disabled={!itemName || !uploadedImage}
+                            >
+                                Next
+                            </Button>
+                        </>
+                    )}
+
+                    {step === 2 && (
+                        <>
+                            <Typography
+                                variant="h6"
+                                sx={{ textAlign: "center", mb: 3, fontWeight: "bold", color: "#333" }}
+                            >
+                                Add Subcategories
+                            </Typography>
+
+                            {/* Sub Category Input */}
+                            <Box sx={{ display: "flex", mb: 2 }}>
+                                <TextField
+                                    fullWidth
+                                    label="Sub Category Name"
+                                    variant="outlined"
+                                    value={subCategoryName}
+                                    onChange={(e) => setSubCategoryName(e.target.value)}
+                                    sx={{ mr: 2 }}
+                                />
+                                <Button variant="contained" color="primary" onClick={handleAddSubCategory}>
+                                    Add
+                                </Button>
+                            </Box>
+
+                            {/* Sub Category List */}
+                            <List>
+                                {subCategory.map((subCategory, index) => (
+                                    <ListItem key={index} sx={{ borderBottom: "1px solid #ddd" }}>
+                                        <ListItemText primary={subCategory} />
+                                        <ListItemSecondaryAction>
+                                            <IconButton
+                                                edge="end"
+                                                aria-label="delete"
+                                                onClick={() => handleRemoveSubCategory(index)}
+                                            >
+                                                <DeleteIcon />
+                                            </IconButton>
+                                        </ListItemSecondaryAction>
+                                    </ListItem>
+                                ))}
+                            </List>
+
+                            {/* Submit Button */}
+                            <Button
+                                variant="contained"
+                                color="success"
+                                fullWidth
+                                sx={{ mt: 3 }}
+                                onClick={handleSubmit}
+                                disabled={subCategory.length === 0}
+                            >
+                                Submit
+                            </Button>
+                        </>
+                    )}
+                </Box>
+            </Modal>
+
 
 
         </Box>
